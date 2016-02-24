b6d8e977f4cf4f92d4ceb68e5b47dbb613899898
antoinedelahaye <l.a.delahaye0902@gmail.com>
2016-02-24 16:53:17 +0100

    Modification CV

--- routes/modifierCv.js
+++ routes/modifierCv.js
@@ -5,6 +5,8 @@
 var models  = require('../models');
 var express = require('express');
 var router = express.Router();
+//Liste des mois
+var mois = ["Janvier","Février","Mars","Avril","Mai","Juin","Juillet","Août","Septembre","Octobre","Novembre","Décembre"];
 
 /* GET home page. */
 router.get('/:idCv', function(req, res, next) {
@@ -19,11 +21,45 @@ router.get('/:idCv', function(req, res, next) {
         ]
     }).then(function(cv){
         console.log(JSON.stringify(cv));
-
-        //Liste des mois
-        var mois = ["Janvier","Février","Mars","Avril","Mai","Juin","Juillet","Août","Septembre","Octobre","Novembre","Décembre"];
-        res.render('modifierCv', { title: 'Modification du CV', cv: cv, mois: mois, session: req.session});
+        res.render('modifierCv', { title: 'Modification du CV', cv: cv, mois: mois, session: req.session, cvid: cv.id});
     });
 });
+
+
+router.post('/', function (req, res) {
+    models.CV.findOne( {
+        where:{id: req.body.cvid},
+        include:[{model: models.Competence_CV},
+            {model: models.Candidat},
+            {model: models.Langue},
+            {model: models.Centre_interet},
+            {model: models.Experience_pro, include : [{model: models.Mission_CV}, {model: models.Contrat_type}]},
+            {model: models.Formation}
+        ]
+    }).then(function(cv) {
+        if (req.body.oldresume != req.body.resumeCv){
+            cv.update({
+                resume: req.body.resumeCv
+            })
+        }
+    });
+    var id = req.body.idformation;
+    console.log(id + "|||||||");
+    models.Formation.findOne( {
+        where:{cvId: req.body.cvid, id:req.body.idformation},
+        include:[{model: models.CV}]
+        }).then(function(formation) {
+        if (req.body.oldanneeFormation != req.body.anneeFormation){
+            console.log("TEST avant");
+            formation.update({
+                annee: parseInt(req.body.anneeFormation)
+            })
+            console.log("TEST APRES");
+        }
+    });
+});
+
+
+
 
 module.exports = router;
