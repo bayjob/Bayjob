a2529ea65fe24d827880a69d393c4d814c186248
antoine-lecoustre <a.lecoustre@gmail.com>
2016-03-15 21:12:33 +0100

    ajout de l'affichage des notifications envoyées

--- routes/notification.js
+++ routes/notification.js
@@ -9,49 +9,66 @@ var mois = ["Janvier", "Février", "Mars", "Avril", "Mai", "Juin", "Juillet", "A
 router.get('/', function (req, res, next) {
 
     if (req.session && req.session.user) {
-        //Requête à exécuter quand le candidat est connecté
-        if (req.session.type == 'C') {
-            models.Notification.findAll({
-                include: [{
-                    model: models.CV,
-                    include: {
-                        model: models.Candidat,
-                        include: {model: models.Utilisateur, where: {id: req.session.user}}
-                    }
-                },
-                    {model: models.Utilisateur, include: {model: models.Recruteur}, where: {type: 'R'}},
-                    {model: models.Offre}]
-            }).then(function (notif) {
-                console.log(JSON.stringify(notif));
-                res.render('notification', {
-                    title: 'Notification',
-                    notifications: notif,
-                    mois: mois,
-                    session: req.session
+        //Requête de récupération des notifications envoyées par l'utilisateur
+        models.Notification.findAll({
+            include : [{model: models.CV, include: {model: models.Candidat, include:{model: models.Utilisateur}}},{model: models.Offre, include: {model: models.Recruteur, include:{model: models.Utilisateur}}}],
+            where : [{UtilisateurId: req.session.user}, {choix: 1}]
+        }).then(function(notifEnvoyees){
+
+            //Requête à exécuter quand le candidat est connecté
+            if (req.session.type == 'C') {
+                //Récupération des notification reçus
+                models.Notification.findAll({
+                    include: [{
+                        model: models.CV,
+                        include: {
+                            model: models.Candidat,
+                            include: {model: models.Utilisateur, where: {id: req.session.user}}
+                        }
+                    },
+                        {model: models.Utilisateur, include: {model: models.Recruteur}, where: {type: 'R'}},
+                        {model: models.Offre}],
+                    where: {choix: 1}
+                }).then(function (notif) {
+                    console.log(JSON.stringify(notif));
+                    console.log(JSON.stringify(notifEnvoyees));
+                    res.render('notification', {
+                        title: 'Notification',
+                        notificationsRecues: notif,
+                        notificationsEnvoyees: notifEnvoyees,
+                        mois: mois,
+                        session: req.session
+                    });
                 });
-            });
-        } else {
-            //Requête à exécuter quand le recruteur est connecté
-            models.Notification.findAll({
-                include: [{
-                    model: models.Offre,
-                    include: {
-                        model: models.Recruteur,
-                        include: {model: models.Utilisateur, where: {id: req.session.user}}
-                    }
-                },
-                    {model: models.Utilisateur, include: {model: models.Candidat}, where: {type: 'C'}},
-                    {model: models.CV}]
-            }).then(function (notif) {
-                console.log(JSON.stringify(notif));
-                res.render('notification', {
-                    title: 'Notification',
-                    notifications: notif,
-                    mois: mois,
-                    session: req.session
+                //Requête à exécuter quand le recruteur est connecté
+            } else {
+                //Récupération des notification reçus
+                models.Notification.findAll({
+                    include: [{
+                        model: models.Offre,
+                        include: {
+                            model: models.Recruteur,
+                            include: {model: models.Utilisateur, where: {id: req.session.user}}
+                        }
+                    },
+                        {model: models.Utilisateur, include: {model: models.Candidat}, where: {type: 'C'}},
+                        {model: models.CV}],
+                    where: {choix: 1}
+                }).then(function (notif) {
+                    console.log(JSON.stringify(notif));
+                    console.log(JSON.stringify(notifEnvoyees));
+                    res.render('notification', {
+                        title: 'Notification',
+                        notificationsRecues: notif,
+                        notificationsEnvoyees: notifEnvoyees,
+                        mois: mois,
+                        session: req.session
+                    });
                 });
-            });
-        }
+            }
+        });
+
+
     } else {
         res.redirect('/login');
     }
