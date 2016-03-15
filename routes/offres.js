de00461df96a4a00e83fe003da369a112b49d4b1
antoine-lecoustre <a.lecoustre@gmail.com>
2016-03-15 16:22:49 +0100

    fix cv + offre

--- routes/offres.js
+++ routes/offres.js
@@ -1,12 +1,12 @@
 /**
  * Created by Achraf on 19/02/2016.
  */
-var models  = require('../models');
+var models = require('../models');
 var express = require('express');
 var router = express.Router();
 
 
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
     var offresUser = [];
 
     var cvsUtilisateurConnecte = null;
@@ -34,8 +34,8 @@ router.get('/', function(req, res, next) {
                 attributes: ['id']
             }).then(function (offre) {
                 models.Offre.findOne({
-                    where:{id: offre.id},
-                    include:[
+                    where: {id: offre.id},
+                    include: [
                         {model: models.Recruteur},
                         {model: models.Departement},
                         {model: models.Contrat_type},
@@ -43,16 +43,20 @@ router.get('/', function(req, res, next) {
                         {model: models.Mission_offre},
                         {model: models.Competence_offre}
                     ]
-                }).then(function(offre){
+                }).then(function (offre) {
                     console.log(JSON.stringify(offre));
 
                     //Liste des mois
-                    var mois = ["Janvier","Février","Mars","Avril","Mai","Juin","Juillet","Août","Septembre","Octobre","Novembre","Décembre"];
-                    res.render('offre', { title: 'offre', offre: offre, mois: mois, session:req.session, cvsUtilisateurConnecte:cvsUtilisateurConnecte});
+                    var mois = ["Janvier", "Février", "Mars", "Avril", "Mai", "Juin", "Juillet", "Août", "Septembre", "Octobre", "Novembre", "Décembre"];
+                    res.render('offre', {
+                        title: 'offre',
+                        offre: offre,
+                        mois: mois,
+                        session: req.session,
+                        cvsUtilisateurConnecte: cvsUtilisateurConnecte
+                    });
                 });
             });
-        } else if (offresUser.length == 0 && notifUtilisateur.length != 0) {
-            res.redirect('/espaceRecruteur');
         } else {
             models.Offre.findOne({
                 attributes: ['id'],
@@ -62,44 +66,53 @@ router.get('/', function(req, res, next) {
                     }
                 }
             }).then(function (offre) {
-                models.Offre.findOne({
-                    where:{id: offre.id},
-                    include:[
-                        {model: models.Recruteur},
-                        {model: models.Departement},
-                        {model: models.Contrat_type},
-                        {model: models.Niveau_etude},
-                        {model: models.Mission_offre},
-                        {model: models.Competence_offre}
-                    ]
-                }).then(function(offre){
-                    console.log(JSON.stringify(offre));
+                if(offre === null){
+                    res.redirect("/espaceCandidat");
+                }else{
+                    models.Offre.findOne({
+                        where: {id: offre.id},
+                        include: [
+                            {model: models.Recruteur},
+                            {model: models.Departement},
+                            {model: models.Contrat_type},
+                            {model: models.Niveau_etude},
+                            {model: models.Mission_offre},
+                            {model: models.Competence_offre}
+                        ]
+                    }).then(function (offre) {
+                        console.log(JSON.stringify(offre));
 
-                    //Liste des mois
-                    var mois = ["Janvier","Février","Mars","Avril","Mai","Juin","Juillet","Août","Septembre","Octobre","Novembre","Décembre"];
-                    res.render('offre', { title: 'offre', offre: offre, mois: mois, session:req.session, cvsUtilisateurConnecte:cvsUtilisateurConnecte});
-                });
+                        //Liste des mois
+                        var mois = ["Janvier", "Février", "Mars", "Avril", "Mai", "Juin", "Juillet", "Août", "Septembre", "Octobre", "Novembre", "Décembre"];
+                        res.render('offre', {
+                            title: 'offre',
+                            offre: offre,
+                            mois: mois,
+                            session: req.session,
+                            cvsUtilisateurConnecte: cvsUtilisateurConnecte
+                        });
+                    });
+                }
             });
-
         }
     });
 });
 
 /* GET home page. */
-router.get('/:idOffres', function(req, res, next) {
+router.get('/:idOffres', function (req, res, next) {
     var cvsUtilisateurConnecte = null;
 
     //Récupération des cv de l'utilisateur connecté
     models.CV.findAll({
-        include : {model: models.Candidat, where : {UtilisateurId: req.session.user}}
-    }).then(function(cvs){
+        include: {model: models.Candidat, where: {UtilisateurId: req.session.user}}
+    }).then(function (cvs) {
         cvsUtilisateurConnecte = cvs;
     });
 
 
     models.Offre.findOne({
-        where:{id: req.params.idOffres},
-        include:[
+        where: {id: req.params.idOffres},
+        include: [
             {model: models.Recruteur},
             {model: models.Departement},
             {model: models.Contrat_type},
@@ -107,15 +120,19 @@ router.get('/:idOffres', function(req, res, next) {
             {model: models.Mission_offre},
             {model: models.Competence_offre}
         ]
-    }).then(function(offre){
+    }).then(function (offre) {
         console.log(JSON.stringify(offre));
 
         //Liste des mois
-        var mois = ["Janvier","Février","Mars","Avril","Mai","Juin","Juillet","Août","Septembre","Octobre","Novembre","Décembre"];
-        res.render('offre', { title: 'offre', offre: offre, mois: mois, session:req.session, cvsUtilisateurConnecte:cvsUtilisateurConnecte});
+        var mois = ["Janvier", "Février", "Mars", "Avril", "Mai", "Juin", "Juillet", "Août", "Septembre", "Octobre", "Novembre", "Décembre"];
+        res.render('offre', {
+            title: 'offre',
+            offre: offre,
+            mois: mois,
+            session: req.session,
+            cvsUtilisateurConnecte: cvsUtilisateurConnecte
+        });
     });
-
-
 });
 
 module.exports = router;
