83bf5bd44da04895e89e2d1dee3403aa97930219
landa0902 <l.a.delahaye0902@gmail.com>
2016-03-17 09:51:46 +0100

    Fix bug

--- models/niveau_etude.js
+++ models/niveau_etude.js
@@ -6,6 +6,7 @@ module.exports = function(sequelize, DataTypes) {
     classMethods: {
       associate: function(models) {
         Niveau_etude.hasMany(models.Offre);
+        Niveau_etude.hasMany(models.CV);
       }
     }
   });
